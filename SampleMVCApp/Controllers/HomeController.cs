58f39e866a2f515190e602f9db084d12fdb3f1f3
notepc2025\development <tigermachine109@yahoo.co.jp>
2025-03-23 17:06:35 +0900

    SourceTreeで出てくるポップアップの調査のため再度コミット

--- SampleMVCApp/Controllers/HomeController.cs
+++ SampleMVCApp/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ public class HomeController : Controller
     {
         //2025.03.23 再度コメント追加
         //2025.03.23 16:47 github接続確認
+        //2025.03.23 17:05 再度確認のため変更
         return View();
     }
 
